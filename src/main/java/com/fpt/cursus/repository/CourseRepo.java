10bf61a51e301cc619308f4c26c71de145769c07
Trần Đăng Quốc Đạt <d872003@gmail.com>
2024-06-12 12:49:19 +0700

    Feature[Chapter-Lesson][CRS-18, CRS-19, CRS-25, CRS-39, CRS-40, CRS-41]: - CRUD Chapter - CRUD Lesson - Update ErrorCode

--- src/main/java/com/fpt/cursus/repository/CourseRepo.java
+++ src/main/java/com/fpt/cursus/repository/CourseRepo.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface CourseRepo extends JpaRepository<Course, Long> {
     List<Course> findAll();
-
+    boolean existsByName(String name);
     Course findCourseById(Long id);
 
 }
