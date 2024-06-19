72439cfd8806d7d7fb513f3b2ecb857e305ddf8a
Trần Đăng Quốc Đạt <d872003@gmail.com>
2024-06-19 11:05:34 +0700

    Update enroll Course

--- src/main/java/com/fpt/cursus/service/UserService.java
+++ src/main/java/com/fpt/cursus/service/UserService.java
@@ -1,15 +1,18 @@
 // File: src/main/java/com/fpt/cursus/service/UserService.java
 package com.fpt.cursus.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fpt.cursus.dto.object.EnrollCourseDto;
 import com.fpt.cursus.dto.request.ChangePasswordDto;
 import com.fpt.cursus.dto.request.LoginReqDto;
 import com.fpt.cursus.dto.request.RegisterReqDto;
 import com.fpt.cursus.dto.request.ResetPasswordDto;
-import com.fpt.cursus.dto.EnrollCourseDto;
 import com.fpt.cursus.dto.response.LoginResDto;
 import com.fpt.cursus.entity.Account;
 import com.fpt.cursus.entity.Course;
 import com.fpt.cursus.entity.Otp;
+import com.fpt.cursus.enums.type.Role;
 import com.fpt.cursus.enums.status.UserStatus;
 import com.fpt.cursus.exception.exceptions.AppException;
 import com.fpt.cursus.exception.exceptions.ErrorCode;
@@ -34,7 +37,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -64,49 +66,38 @@ public class UserService {
     private AccountUtil accountUtil;
 
     @Autowired
-    private EmailUtil emailUtil;
-
-    @Autowired
     private Regex regex;
 
     @Autowired
     private OtpService otpService;
 
-    @Autowired
-    private MapperUtil mapperUtil;
 
     public Account register(RegisterReqDto registerReqDTO) {
         if (!regex.isPhoneValid(registerReqDTO.getPhone())) {
             throw new AppException(ErrorCode.PHONE_NOT_VALID);
         }
-
         Account account = new Account();
         account.setUsername(registerReqDTO.getUsername());
         account.setPassword(passwordEncoder.encode(registerReqDTO.getPassword()));
         account.setEmail(registerReqDTO.getEmail());
         account.setFullName(registerReqDTO.getFullName());
-        account.setRole(registerReqDTO.getRole());
+        if (registerReqDTO.getRole().equals(Role.INSTRUCTOR)) {
+            account.setRole(Role.PRE_INSTRUCTOR);
+        } else {
+            account.setRole(registerReqDTO.getRole());
+        }
         account.setPhone(registerReqDTO.getPhone());
-        account.setStatus(UserStatus.ACTIVE);
-        List<EnrollCourseDto> enrolledCourses = new ArrayList<>();
-        EnrollCourseDto course1 = new EnrollCourseDto();
-        course1.setCourseName("Course 1");
-        EnrollCourseDto course2 = new EnrollCourseDto();
-        course2.setCourseName("Course 2");
-        enrolledCourses.add(course1);
-        enrolledCourses.add(course2);
-        MapperUtil mapperUtil = new MapperUtil();
-        String enrolledCourseJson = mapperUtil.serializeCourseList(enrolledCourses);
-        account.setEnrolledCourseJson(enrolledCourseJson);
-
+        account.setGender(registerReqDTO.getGender());
+        account.setCvLink(registerReqDTO.getCvLink());
+        account.setAvatar(registerReqDTO.getAvatar());
+        account.setStatus(UserStatus.INACTIVE);
         return accountRepo.save(account);
+        //
     }
 
     public LoginResDto login(LoginReqDto loginReqDto) {
         try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginReqDto.getUsername(), loginReqDto.getPassword())
-            );
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginReqDto.getUsername(), loginReqDto.getPassword()));
 
             Account account = (Account) authentication.getPrincipal();
 
@@ -142,30 +133,32 @@ public class UserService {
     }
 
     public void verifyAccount(String email, String otp) {
-        Account account = accountRepo.findByEmail(email)
-                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
-        Otp userOtp = otpRepo.findMailByEmail(email);
+        Otp userOtp = otpRepo.findOtpByEmailAndValid(email, true);
         if (validateOtp(userOtp, otp)) {
+            Account account = accountRepo.findByEmail(email).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
             account.setStatus(UserStatus.ACTIVE);
+            otpRepo.updateOldOtps(email);
             accountRepo.save(account);
-            LocalDateTime expiryTime = LocalDateTime.now().minusMinutes(2);
-            otpRepo.deleteOldOtps(email, expiryTime);
         } else {
             throw new AppException(ErrorCode.OTP_INVALID);
         }
     }
 
+    public void verifyInstructorById(Long id) {
+        Account account = accountRepo.findById(id).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        account.setRole(Role.INSTRUCTOR);
+        accountRepo.save(account);
+    }
+
     public void regenerateOtp(String email) {
-        LocalDateTime expiryTime = LocalDateTime.now().minusMinutes(2);
-        otpRepo.deleteOldOtps(email, expiryTime);
-        String otp = String.valueOf(otpService.generateOtp());
+        otpRepo.updateOldOtps(email);
+        String otp = otpService.generateOtp();
         otpService.sendOtpEmail(email, otp);
         otpService.saveOtp(email, otp);
     }
 
     public void deleteAccount(String username) {
-        Account account = accountRepo.findByUsername(username)
-                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        Account account = accountRepo.findByUsername(username).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
 
         account.setStatus(UserStatus.DELETED);
         accountRepo.save(account);
@@ -197,90 +190,30 @@ public class UserService {
         if (!resetPasswordDto.getPassword().equals(resetPasswordDto.getConfirmPassword())) {
             throw new AppException(ErrorCode.PASSWORD_NOT_MATCH);
         }
-        Account account = accountRepo.findByEmail(email)
-                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
-        Otp userOtp = otpRepo.findMailByEmail(email);
+        Account account = accountRepo.findByEmail(email).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        Otp userOtp = otpRepo.findOtpByEmailAndValid(email, true);
         if (validateOtp(userOtp, otp)) {
             account.setPassword(passwordEncoder.encode(resetPasswordDto.getPassword()));
+            otpRepo.updateOldOtps(email);
             accountRepo.save(account);
-            LocalDateTime expiryTime = LocalDateTime.now().minusMinutes(2);
-            otpRepo.deleteOldOtps(email, expiryTime);
         } else {
             throw new AppException(ErrorCode.OTP_INVALID);
         }
     }
 
     public void forgotPassword(String email) {
-        LocalDateTime expiryTime = LocalDateTime.now().minusMinutes(2);
-        otpRepo.deleteOldOtps(email, expiryTime);
         String otp = otpService.generateOtp();
         otpService.sendResetPasswordEmail(email, otp);
         otpService.saveOtp(email, otp);
     }
 
     private boolean validateOtp(Otp userOtp, String otp) {
-        if (Duration.between(userOtp.getOtpGeneratedTime(), LocalDateTime.now()).getSeconds() < (2 * 60)) {
+        if (Duration.between(userOtp.getOtpGeneratedTime(), LocalDateTime.now()).getSeconds() < (20)) {
             return userOtp.getOtp().equals(otp);
         } else {
             throw new AppException(ErrorCode.OTP_EXPIRED);
         }
     }
 
-
-    //Detailed Information
-    public List<Course> getEnrolledCoursesByUsername(String username) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-            Account account_check = (Account) authentication.getPrincipal();
-            if (!account_check.getUsername().equals(username)) {
-               throw new AppException(ErrorCode.USER_UNAUTHORIZED);
-            }else{
-                Account account = (Account) this.accountRepo.findByUsername(username).orElseThrow(() -> {
-                    throw new AppException(ErrorCode.USER_NOT_FOUND);
-                });
-                if (account.getEnrolledCourseJson() == null || account.getEnrolledCourseJson().isEmpty()) {
-                    throw new AppException(ErrorCode.USER_ENROLLED_EMPTY);
-                } else {
-                    List<EnrollCourseDto> enrolledCourses = mapperUtil.deserializeCourseList(account.getEnrolledCourseJson(), EnrollCourseDto.class);
-                    List<String> courseNames = enrolledCourses.stream().map(EnrollCourseDto::getCourseName).collect(Collectors.toList());
-                    List<Course> courses = courseRepo.findByNameIn(courseNames);
-                    return courses;
-                }
-            }
-        }else{
-                throw new AppException(ErrorCode.UNCATEGORIZED_ERROR);
-        }
-    }
-
-    //Only Name of course
-//    public List<?> getEnrolledCoursesByUsernameNotDetailed(String username) {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-//            Account account_check = (Account) authentication.getPrincipal();
-//            if (!account_check.getUsername().equals(username)) {
-//                throw new AppException(ErrorCode.USER_UNAUTHORIZED);
-//            }else{
-//                Account account = (Account) this.accountRepo.findByUsername(username).orElseThrow(() -> {
-//                    throw new AppException(ErrorCode.USER_NOT_FOUND);
-//                });
-//                if (account.getEnrolledCourseJson() == null || account.getEnrolledCourseJson().isEmpty()) {
-//                    throw new AppException(ErrorCode.USER_ENROLLED_EMPTY);
-//                } else {
-//                    List<EnrollCourseDto> enrolledCourses = mapperUtil.deserializeCourseList(account.getEnrolledCourseJson(), EnrollCourseDto.class);
-//                    List<String> courseNames = enrolledCourses.stream().map(EnrollCourseDto::getCourseName).collect(Collectors.toList());
-//                    List<Course> courses = courseRepo.findByNameIn(courseNames);
-//                   List<EnrollCourseDto> courseResponse = courses.stream().map(course -> new EnrollCourseDto(
-//                           course.getName(),
-//                           course.getCategory().toString(),
-//                           course.getPrice(),
-//                           course.getRating()
-//                   )).collect(Collectors.toList());
-//                   return courseResponse;
-//                }
-//            }
-//        }else{
-//            throw new AppException(ErrorCode.UNCATEGORIZED_ERROR);
-//        }
-//    }
 }
 
