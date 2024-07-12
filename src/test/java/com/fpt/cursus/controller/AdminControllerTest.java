a1eae421ae667eb92f747fc9febd737fad507506
Krit Dat <113153168+NHKritDat@users.noreply.github.com>
2024-07-12 15:14:56 +0700

    unit test: CRS-44.34: Update Unit Testing Admin Controller

--- src/test/java/com/fpt/cursus/controller/AdminControllerTest.java
+++ src/test/java/com/fpt/cursus/controller/AdminControllerTest.java
@@ -7,6 +7,7 @@ import com.fpt.cursus.entity.Course;
 import com.fpt.cursus.enums.CourseStatus;
 import com.fpt.cursus.enums.InstructorStatus;
 import com.fpt.cursus.enums.Role;
+import com.fpt.cursus.enums.UserStatus;
 import com.fpt.cursus.service.AccountService;
 import com.fpt.cursus.service.CourseService;
 import com.fpt.cursus.util.ApiResUtil;
@@ -29,14 +30,12 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(AdminController.class)
 @ContextConfiguration(classes = {
-        ApiResUtil.class,
         AccountService.class,
         CourseService.class
 })
@@ -47,9 +46,6 @@ class AdminControllerTest {
 
     @MockBean
     private CourseService courseService;
-
-    @MockBean
-    private ApiResUtil apiResUtil;
 
     @Autowired
     private MockMvc mockMvc;
@@ -65,189 +61,164 @@ class AdminControllerTest {
     }
 
     @Test
-    void testVerifyAccount_Success() throws Exception {
+    void approveInstructorSuccess() throws Exception {
         //given
-        InstructorStatus instructorStatus = InstructorStatus.APPROVED;
-        long id = 1;
-        String successMessage = "Verify instructor successfully.";
-        ApiRes<Object> apiRes = new ApiRes<>();
-        apiRes.setStatus(null);
-        apiRes.setMessage(successMessage);
-        apiRes.setCode(null);
-        apiRes.setData(null);
-        //when
-        when(apiResUtil.returnApiRes(any(), any(), any(), any()))
-                .thenReturn(apiRes);
-        //then
-        mockMvc.perform(patch("/admin/verify-instructor")
-                        .param("id", String.valueOf(id))
-                        .param("status", String.valueOf(instructorStatus)))
-                .andExpectAll(status().isOk(),
-                        jsonPath("$.message").value(successMessage));
-    }
-
-    @Test
-    void testViewDraftCourse_Success() throws Exception {
-        //given
-        String sortBy = "name";
-        int offset = 0;
-        int limit = 10;
-        Pageable pageable = PageRequest.of(offset, limit, Sort.Direction.DESC, sortBy);
-        Course course = new Course();
-        course.setName("course1");
-        Course course2 = new Course();
-        course2.setName("course2");
-        List<Course> courseList = new ArrayList<>();
-        courseList.add(course);
-        courseList.add(course2);
-        Page<Course> page = new PageImpl<>(courseList, pageable, courseList.size());
-        ApiRes<Object> apiRes = new ApiRes<>();
-        apiRes.setStatus(null);
-        apiRes.setMessage("");
-        apiRes.setCode(null);
-        apiRes.setData(page);
-        //when
-        when(courseService.getCourseByStatus(any(CourseStatus.class),
-                anyInt(), anyInt(), anyString()))
-                .thenReturn(page);
-        when(apiResUtil.returnApiRes(any(), any(), any(), any()))
-                .thenReturn(apiRes);
-        //then
-        mockMvc.perform(get("/course/view-draft-course")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .param("sortBy", sortBy)
-                        .param("offset", String.valueOf(offset))
-                        .param("pageSize", String.valueOf(limit)))
-                .andExpectAll(status().isOk(),
-                        jsonPath("$.data").hasJsonPath());
-    }
-
-    @Test
-    void testViewDraftCourse_NoParam_Success() throws Exception {
-        //given
-        String sortBy = "name";
-        int offset = 0;
-        int limit = 10;
-        Pageable pageable = PageRequest.of(offset, limit, Sort.Direction.DESC, sortBy);
-        Course course = new Course();
-        course.setName("course1");
-        Course course2 = new Course();
-        course2.setName("course2");
-        List<Course> courseList = new ArrayList<>();
-        courseList.add(course);
-        courseList.add(course2);
-        Page<Course> page = new PageImpl<>(courseList, pageable, courseList.size());
-        ApiRes<Object> apiRes = new ApiRes<>();
-        apiRes.setStatus(null);
-        apiRes.setMessage("");
-        apiRes.setCode(null);
-        apiRes.setData(page);
-        //when
-        when(courseService.getCourseByStatus(any(CourseStatus.class),
-                anyInt(), anyInt(), anyString()))
-                .thenReturn(page);
-        when(apiResUtil.returnApiRes(any(), any(), any(), any()))
-                .thenReturn(apiRes);
-        //then
-        mockMvc.perform(get("/course/view-draft-course")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpectAll(status().isOk(),
-                        jsonPath("$.data").hasJsonPath());
-    }
-
-    @Test
-    void testVerifyCourse_Success() throws Exception {
-        //given
-        CourseStatus courseStatus = CourseStatus.PUBLISHED;
-        long id = 1;
-        String successMessage = "Verify course successfully.";
-        ApiRes<Object> apiRes = new ApiRes<>();
-        apiRes.setStatus(null);
-        apiRes.setMessage(successMessage);
-        apiRes.setCode(null);
-        apiRes.setData(null);
-        //when
-        when(apiResUtil.returnApiRes(any(), any(), any(), any()))
-                .thenReturn(apiRes);
-        //then
-        mockMvc.perform(patch("/admin/verify-course")
-                        .param("id", String.valueOf(id))
-                        .param("status", String.valueOf(courseStatus)))
-                .andExpectAll(status().isOk(),
-                        jsonPath("$.message").value(successMessage));
-    }
-
-    @Test
-    void testViewVerifyingInstructor_Success() throws Exception {
-        //given
-        InstructorStatus instructorStatus = InstructorStatus.APPROVED;
-
         Account account = new Account();
-        account.setUsername("username");
+        account.setId(1L);
         account.setRole(Role.INSTRUCTOR);
-        account.setInstructorStatus(instructorStatus);
-        Account account2 = new Account();
-        account2.setUsername("username2");
-        account2.setRole(Role.INSTRUCTOR);
-        account2.setInstructorStatus(instructorStatus);
-        List<Account> accountList = new ArrayList<>();
-        accountList.add(account);
-        accountList.add(account2);
+        //when
+        when(accountService.approveInstructorById(anyLong(), any(InstructorStatus.class)))
+                .thenReturn(account);
+        //then
+        mockMvc.perform(patch("/admin/approve-instructor")
+                .param("id", "1")
+                .param("status", InstructorStatus.APPROVED.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(status().isOk(),
+                        content().json(objectMapper.writeValueAsString(account)));
+    }
 
-        ApiRes<Object> apiRes = new ApiRes<>();
-        apiRes.setStatus(null);
-        apiRes.setMessage(null);
-        apiRes.setCode(null);
-        apiRes.setData(accountList);
+    @Test
+    void viewDraftCourseSuccess() throws Exception {
+        //given
+        List<Course> courses = new ArrayList<>();
+        Course course = new Course();
+        course.setId(1L);
+        course.setStatus(CourseStatus.DRAFT);
+        courses.add(course);
+
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("id"));
+        Page<Course> page = new PageImpl<>(courses, pageable, courses.size());
+        //when
+        when(courseService.getCourseByStatus(any(CourseStatus.class), anyInt(), anyInt(), anyString()))
+                .thenReturn(page);
+        //then
+        mockMvc.perform(get("/admin/view-draft-course")
+                        .param("sortBy", "id")
+                        .param("offset", "1")
+                        .param("pageSize", "10"))
+                .andExpectAll(status().isOk(),
+                        content().json(objectMapper.writeValueAsString(page)));
+    }
+
+    @Test
+    void viewAllCourseSuccess() throws Exception {
+        //given
+        List<Course> courses = new ArrayList<>();
+        Course course = new Course();
+        course.setId(1L);
+        courses.add(course);
+
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("id"));
+        Page<Course> page = new PageImpl<>(courses, pageable, courses.size());
+        //when
+        when(courseService.getAllCourse(anyInt(), anyInt(), anyString()))
+                .thenReturn(page);
+        //then
+        mockMvc.perform(get("/admin/view-all-course")
+                .param("sortBy", "id")
+                .param("offset", "1")
+                .param("pageSize", "10")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(status().isOk(),
+                        content().json(objectMapper.writeValueAsString(page)));
+    }
+
+    @Test
+    void approveCourseSuccess() throws Exception {
+        //given
+        Course course = new Course();
+        course.setId(1L);
+        course.setStatus(CourseStatus.PUBLISHED);
+        //when
+        when(courseService.approveCourseById(anyLong(), any(CourseStatus.class)))
+                .thenReturn(course);
+        //then
+        mockMvc.perform(patch("/admin/approve-course")
+                .param("id", "1")
+                .param("status", CourseStatus.PUBLISHED.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(status().isOk(),
+                        content().json(objectMapper.writeValueAsString(course)));
+    }
+
+    @Test
+    void viewVerifyingInstructorSuccess() throws Exception {
+        //given
+        List<Account> accounts = new ArrayList<>();
+        Account account = new Account();
+        account.setId(1L);
+        account.setRole(Role.INSTRUCTOR);
+        accounts.add(account);
         //when
         when(accountService.getInstructorByInstStatus(any(InstructorStatus.class)))
-                .thenReturn(accountList);
-        when(apiResUtil.returnApiRes(any(), any(), any(), anyList()))
-                .thenReturn(apiRes);
+                .thenReturn(accounts);
         //then
-        mockMvc.perform(get("/admin/view-verifying-instructor")
-                        .param("status", String.valueOf(instructorStatus)))
+        mockMvc.perform(get("/admin/view-instructor")
+                .param("status", InstructorStatus.APPROVED.toString())
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpectAll(status().isOk(),
-                        jsonPath("$.data").hasJsonPath());
+                        content().json(objectMapper.writeValueAsString(accounts)));
     }
 
     @Test
-    void testDeleteAccount_Success() throws Exception {
+    void viewListSuccess() throws Exception {
         //given
-        String username = "username";
-        String successMessage = "Delete account successfully.";
-        ApiRes<Object> apiRes = new ApiRes<>();
-        apiRes.setStatus(null);
-        apiRes.setMessage(successMessage);
-        apiRes.setCode(null);
-        apiRes.setData(null);
+        List<Account> accounts = new ArrayList<>();
+        Account account = new Account();
+        account.setId(1L);
+        account.setRole(Role.INSTRUCTOR);
+        accounts.add(account);
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("id"));
+        Page<Account> page = new PageImpl<>(accounts, pageable, accounts.size());
         //when
-        when(apiResUtil.returnApiRes(any(), any(), any(), any()))
-                .thenReturn(apiRes);
+        when(accountService.getListOfStudentAndInstructor(any(Role.class), anyInt(), anyInt(), anyString()))
+                .thenReturn(page);
         //then
-        mockMvc.perform(delete("/admin/delete-account")
-                        .param("username", username))
+        mockMvc.perform(get("/admin/view-instructor-and-student")
+                .param("role", Role.INSTRUCTOR.toString())
+                .param("sortBy", "id")
+                .param("offset", "1")
+                .param("pageSize", "10")
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpectAll(status().isOk(),
-                        jsonPath("$.message").value(successMessage));
+                        content().json(objectMapper.writeValueAsString(page)));
     }
 
     @Test
-    void testSetAdmin_Success() throws Exception {
+    void setStatusAccountSuccess() throws Exception {
         //given
-        String email = "email";
-        String successMessage = "Set admin role successfully.";
-        ApiRes<Object> apiRes = new ApiRes<>();
-        apiRes.setStatus(null);
-        apiRes.setMessage(successMessage);
-        apiRes.setCode(null);
-        apiRes.setData(null);
+        Account account = new Account();
+        account.setId(1L);
+        account.setRole(Role.STUDENT);
+        account.setStatus(UserStatus.ACTIVE);
         //when
-        when(apiResUtil.returnApiRes(any(), any(), any(), any()))
-                .thenReturn(apiRes);
+        when(accountService.setStatusAccount(anyString(), any(UserStatus.class)))
+                .thenReturn(account);
+        //then
+        mockMvc.perform(delete("/admin/set-status-account")
+                .param("username", "username")
+                .param("status", UserStatus.ACTIVE.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(status().isOk(),
+                        content().json(objectMapper.writeValueAsString(account)));
+    }
+
+    @Test
+    void setAdminSuccess() throws Exception {
+        //given
+        Account account = new Account();
+        account.setId(1L);
+        account.setEmail("test@test.com");
+        account.setRole(Role.ADMIN);
+        //when
+        when(accountService.setAdmin(anyString()))
+                .thenReturn(account);
         //then
         mockMvc.perform(patch("/admin/set-admin")
-                        .param("email", email))
+                .param("email", "test@test.com")
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpectAll(status().isOk(),
-                        jsonPath("$.message").value(successMessage));
+                        content().json(objectMapper.writeValueAsString(account)));
     }
 }
