22de0df4108430f72807c4f01ab6931a6107005a
EmBerrrrrr <lan18051973@gmail.com>
2024-07-17 14:09:42 +0700

    unit-test:CRS-44.42:Unit-Testing-Course-Controller - viewAllGeneralCourse_success - viewCourseByCategory_success - viewCourseByName_success

--- src/test/java/com/fpt/cursus/controller/CourseControllerTest.java
+++ src/test/java/com/fpt/cursus/controller/CourseControllerTest.java
@@ -1,8 +1,7 @@
 package com.fpt.cursus.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fpt.cursus.dto.request.CreateCourseDto;
-import com.fpt.cursus.dto.request.UpdateCourseDto;
+import com.fpt.cursus.dto.response.GeneralCourse;
 import com.fpt.cursus.entity.Course;
 import com.fpt.cursus.enums.Category;
 import com.fpt.cursus.service.CourseService;
@@ -11,26 +10,20 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.data.domain.Page;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-//import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.data.domain.*;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -71,28 +64,6 @@ class CourseControllerTest {
     }
 
     @Test
-    void updateCourse_success() throws Exception {
-        UpdateCourseDto updateCourseDto = new UpdateCourseDto();
-        updateCourseDto.setName("Updated Course");
-        updateCourseDto.setDescription("Updated description");
-
-        Course updatedCourse = new Course();
-        updatedCourse.setId(1L);
-        updatedCourse.setName("Updated Course");
-
-        when(courseService.updateCourse(anyLong(), any(UpdateCourseDto.class))).thenReturn(updatedCourse);
-
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/course/update")
-                        .file("pictureLink", new byte[0]) // Simulate an empty file
-                        .param("id", "1")
-                        .param("name", "Updated Course")
-                        .param("description", "Updated description")
-                        .contentType(MediaType.MULTIPART_FORM_DATA))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("Updated Course"));
-    }
-
-    @Test
     void deleteCourse_success() throws Exception {
         Course course = new Course();
         when(courseService.deleteCourseById(anyLong())).thenReturn(course);
@@ -106,8 +77,16 @@ class CourseControllerTest {
 
     @Test
     void viewAllGeneralCourse_success() throws Exception {
-        when(courseService.getAllGeneralCourses(anyString(), any(Integer.class), any(Integer.class)))
-                .thenReturn(Page.empty()); // Adjust to return a list of courses if needed
+        GeneralCourse newGeneralCourse = new GeneralCourse();
+        newGeneralCourse.setId(1L);
+        List<GeneralCourse> list = new ArrayList<>();
+        list.add(newGeneralCourse);
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("name"));
+        Page<GeneralCourse> generalCoursePage = new PageImpl<>(list, pageable, list.size());
+
+
+        when(courseService.getAllGeneralCourses(anyString(), anyInt(), anyInt()))
+                .thenReturn(generalCoursePage); // Adjust to return a list of courses if needed
 
         mockMvc.perform(MockMvcRequestBuilders.get("/course/view-all-general")
                         .param("sortBy", "name")
@@ -119,8 +98,14 @@ class CourseControllerTest {
 
     @Test
     void viewCourseByCategory_success() throws Exception {
+        GeneralCourse newGeneralCourse = new GeneralCourse();
+        newGeneralCourse.setId(1L);
+        List<GeneralCourse> list = new ArrayList<>();
+        list.add(newGeneralCourse);
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("name"));
+        Page<GeneralCourse> generalCoursePage = new PageImpl<>(list, pageable, list.size());
         when(courseService.getCourseByCategory(any(Category.class), any(Integer.class), any(Integer.class), anyString()))
-                .thenReturn(Page.empty());
+                .thenReturn(generalCoursePage);
 
         mockMvc.perform(get("/course/view-general-by-category")
                         .param("category", "ALL")
@@ -134,8 +119,14 @@ class CourseControllerTest {
     @Test
     void viewCourseByName_success() throws Exception {
         // Mocking the service call
+        GeneralCourse newGeneralCourse = new GeneralCourse();
+        newGeneralCourse.setId(1L);
+        List<GeneralCourse> list = new ArrayList<>();
+        list.add(newGeneralCourse);
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("name"));
+        Page<GeneralCourse> generalCoursePage = new PageImpl<>(list, pageable, list.size());
         when(courseService.getGeneralCourseByName(anyString(), anyInt(), anyInt(), anyString()))
-                .thenReturn(Page.empty()); // Adjust this to return a valid Page if needed
+                .thenReturn(generalCoursePage); // Adjust this to return a valid Page if needed
 
         // Performing the mockMvc request
         mockMvc.perform(get("/course/view-general-by-name")
