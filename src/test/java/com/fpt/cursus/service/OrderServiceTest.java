af5af0dd37bc6ed7d83d118a6c9da8f32ee5f59b
Krit Dat <113153168+NHKritDat@users.noreply.github.com>
2024-07-24 14:58:13 +0700

    unit test: CRS-44.60: Unit Testing Update Order Service -  testFindAllByIdInSuccess() -  testFindAllByIdInFail() -  testOrderSuccessSuccess() -  testCreatePaymentUrl() - testCreatePaymentUrlCourseNotFound()

--- src/test/java/com/fpt/cursus/service/OrderServiceTest.java
+++ src/test/java/com/fpt/cursus/service/OrderServiceTest.java
@@ -3,10 +3,13 @@ package com.fpt.cursus.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fpt.cursus.dto.request.PaymentDto;
 import com.fpt.cursus.entity.Account;
+import com.fpt.cursus.entity.Course;
 import com.fpt.cursus.entity.Orders;
+import com.fpt.cursus.entity.OrdersDetail;
 import com.fpt.cursus.enums.OrderStatus;
 import com.fpt.cursus.exception.exceptions.AppException;
 import com.fpt.cursus.exception.exceptions.ErrorCode;
+import com.fpt.cursus.repository.OrdersDetailRepo;
 import com.fpt.cursus.repository.OrdersRepo;
 import com.fpt.cursus.service.impl.OrderServiceImpl;
 import com.fpt.cursus.util.AccountUtil;
@@ -16,37 +19,33 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
-    @InjectMocks
-    OrderServiceImpl orderService;
-
-    @Mock
-    AccountUtil accountUtil;
-
-    @Mock
-    OrdersRepo ordersRepo;
-
-    @Mock
-    CourseService courseService;
-
-    @Mock
-    EnrollCourseService enrollCourseService;
-
     @Mock
     ObjectMapper objectMapper;
-
+    @InjectMocks
+    private OrderServiceImpl orderService;
+    @Mock
+    private AccountUtil accountUtil;
+    @Mock
+    private OrdersRepo ordersRepo;
+    @Mock
+    private CourseService courseService;
+    @Mock
+    private EnrollCourseService enrollCourseService;
+    @Mock
+    private OrdersDetailRepo ordersDetailRepo;
     private Orders orders;
     private Account account;
     private PaymentDto paymentDto;
@@ -59,8 +58,10 @@ class OrderServiceTest {
 
         orders = new Orders();
         orders.setId(1L);
+        orders.setCreatedBy("admin");
 
         paymentDto = new PaymentDto();
+
     }
 
     private void setField(Object object, String fieldName, Object value)
@@ -89,74 +90,6 @@ class OrderServiceTest {
                 ErrorCode.ORDER_CART_NULL.getMessage());
     }
 
-//    @Test
-//    void createPaymentUrlSuccess() throws NoSuchFieldException, IllegalAccessException {
-//        //given
-//        List<Long> courseIds = new ArrayList<>();
-//        courseIds.add(1L);
-//        paymentDto.setCourseId(courseIds);
-//        List<Course> courses = new ArrayList<>();
-//        Course newCourse = new Course();
-//        newCourse.setPrice(100.0);
-//        courses.add(newCourse);
-//        setField(orderService, "tmnCode", "9KM2MRF7");
-//        setField(orderService, "secretKey", "PMRS00LV60QVIJQNIW23P67UXP1RUAFB");
-//        setField(orderService, "currCode", "VND");
-//        setField(orderService, "vnpUrl", "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html");
-//        setField(orderService, "returnUrl", "http://localhost:8080/order/update-status");
-//        //when
-//        when(courseService.getCourseByIdsIn(any())).thenReturn(courses);
-//        when(accountUtil.getCurrentAccount()).thenReturn(account);
-//        when(ordersRepo.save(any())).thenReturn(orders);
-//        when(accountUtil.getCurrentAccount()).thenReturn(account);
-//        when(ordersRepo.save(any(Orders.class))).thenReturn(orders);
-//        //then
-//        ResponseEntity<String> response = orderService.createPaymentUrl(paymentDto);
-//
-//        assertNotNull(response);
-//        assertTrue(Objects.requireNonNull(
-//                response.getBody()).contains("https://sandbox.vnpayment.vn/paymentv2/vpcpay.html")
-//        );
-//    }
-//
-//    @Test
-//    void setOrderSuccessSuccess() {
-//        //given
-//        List<Long> courseIds = new ArrayList<>();
-//        courseIds.add(1L);
-//        courseIds.add(2L);
-//        double price = 100.0;
-//        //when
-//        when(accountUtil.getCurrentAccount()).thenReturn(account);
-//        when(ordersRepo.save(any(Orders.class))).thenReturn(orders);
-//        //then
-//        orderService.setOrder(orders, courseIds, price);
-//
-//        assertEquals(account.getUsername(), orders.getCreatedBy());
-//        assertNotNull(orders.getCreatedDate());
-//        assertEquals(OrderStatus.PENDING, orders.getStatus());
-//        assertEquals(price, orders.getPrice());
-//        assertEquals("[1,2]", orders.getOrderCourseJson());
-//    }
-
-//    @Test
-//    void orderSuccess() {
-//        //given
-//        String txnRef = "1";
-//        String responseCode = "00";
-//        orders.setOrderCourseJson("[1,2]");
-//        orders.setStatus(OrderStatus.PENDING);
-//        orders.setCreatedBy(account.getUsername());
-//        //when
-//        when(ordersRepo.findOrdersById(anyLong())).thenReturn(orders);
-//        when(ordersRepo.save(any(Orders.class))).thenReturn(orders);
-//        //then
-//        orderService.orderSuccess(txnRef, responseCode);
-//
-//        assertEquals(OrderStatus.PAID, orders.getStatus());
-//        assertEquals(List.of(1L, 2L), orders.getOrderCourse());
-//    }
-
     @Test
     void orderSuccessFailResponseCode() {
         //given
@@ -184,5 +117,94 @@ class OrderServiceTest {
         assertThrows(AppException.class,
                 () -> orderService.orderSuccess(txnRef, responseCode),
                 ErrorCode.ORDER_FAIL.getMessage());
+    }
+
+    @Test
+    void testFindAllByIdInSuccess() {
+        //given
+        List<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        List<OrdersDetail> ordersDetails = new ArrayList<>();
+        OrdersDetail ordersDetail = new OrdersDetail();
+        ordersDetail.setId(1L);
+        ordersDetails.add(ordersDetail);
+        //when
+        when(ordersDetailRepo.findAllByIdIn(ids)).thenReturn(ordersDetails);
+        //then
+        List<OrdersDetail> result = orderService.findAllByIdIn(ids);
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void testFindAllByIdInFail() {
+        //given
+        List<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        //when
+        when(ordersDetailRepo.findAllByIdIn(ids)).thenReturn(new ArrayList<>());
+        //then
+        assertThrows(AppException.class,
+                () -> orderService.findAllByIdIn(ids),
+                ErrorCode.ORDER_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    void testOrderSuccessSuccess() {
+        //given
+        String txnRef = "1";
+        String responseCode = "00";
+        orders.setOrderCourseJson(List.of("1").toString());
+        orders.setOrdersDetails(List.of(new OrdersDetail(), new OrdersDetail()));
+        //when
+        when(ordersRepo.findOrdersById(anyLong())).thenReturn(orders);
+        when(ordersRepo.save(any(Orders.class))).thenReturn(orders);
+        //then
+        Orders result = orderService.orderSuccess(txnRef, responseCode);
+
+        assertEquals(OrderStatus.PAID, result.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentUrl() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        List<Long> courseIds = new ArrayList<>();
+        courseIds.add(1L);
+        paymentDto.setCourseId(courseIds);
+        List<Course> courses = new ArrayList<>();
+        Course course = new Course();
+        course.setPrice(1000);
+        courses.add(course);
+
+        setField(orderService, "tmnCode", "tmnCode");
+        setField(orderService, "secretKey", "secretKey");
+        setField(orderService, "currCode", "VND");
+        setField(orderService, "vnpUrl", "vnpUrl");
+        setField(orderService, "returnUrl", "returnUrl");
+        //when
+        when(accountUtil.getCurrentAccount()).thenReturn(account);
+        when(courseService.getCourseByIdsIn(anyList())).thenReturn(courses);
+        when(courseService.getCourseById(anyLong())).thenReturn(course);
+
+        //then
+        ResponseEntity<String> result = orderService.createPaymentUrl(paymentDto);
+
+        verify(ordersRepo, times(2)).save(any(Orders.class));
+        verify(ordersDetailRepo, times(1)).saveAll(anyList());
+        assertTrue(result.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    void testCreatePaymentUrlCourseNotFound() {
+        //given
+        List<Long> courseIds = new ArrayList<>();
+        courseIds.add(1L);
+        paymentDto.setCourseId(courseIds);
+        //when
+        when(courseService.getCourseByIdsIn(anyList())).thenReturn(null);
+        //then
+        assertThrows(AppException.class,
+                () -> orderService.createPaymentUrl(paymentDto),
+                ErrorCode.COURSE_NOT_FOUND.getMessage());
     }
 }
