6bf6e10d43a68079fc8cd2adf47efbd68201e41b
redfire3012001 <68159346+redfire3012001@users.noreply.github.com>
2024-07-25 19:31:34 +0700

    unit-test: CRS-44.51->44.55: ExceptionHandler,FileUtil,TokenHandler

---
+++ src/test/java/com/fpt/cursus/exception/handler/GlobalExceptionHandlerTest.java
@@ -0,0 +1,146 @@
+package com.fpt.cursus.exception.handler;
+
+import com.fpt.cursus.dto.response.ApiRes;
+import com.fpt.cursus.exception.exceptions.AppException;
+import com.fpt.cursus.exception.exceptions.ErrorCode;
+import com.fpt.cursus.util.ApiResUtil;
+import com.google.firebase.auth.FirebaseAuthException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GlobalExceptionHandlerTest {
+
+    @Mock
+    private ApiResUtil apiResUtil;
+
+    @InjectMocks
+    private GlobalExceptionHandler globaExceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void handleAppExceptionTest() {
+
+        //Given
+        AppException mockAppException = new AppException(ErrorCode.UNCATEGORIZED_ERROR);
+        var mockApiRes = new ApiRes<>();
+        mockApiRes.setStatus(false);
+        mockApiRes.setCode(HttpStatus.BAD_REQUEST.value());
+        mockApiRes.setMessage(mockAppException.getMessage());
+        mockApiRes.setData(null);
+
+        //When
+        when(apiResUtil.returnApiRes(false, mockAppException.getErrorCode().getCode(), mockAppException.getMessage(), null)).thenReturn(mockApiRes);
+        ResponseEntity<?> responseEntity = globaExceptionHandler.handleAppException(mockAppException);
+
+        //Then
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.BAD_REQUEST);
+        assertEquals(responseEntity.getBody(), mockApiRes);
+    }
+
+    @Test
+    void handleDataIntegrityViolationExceptionTest() {
+
+        //Given
+        DataIntegrityViolationException mockException = new DataIntegrityViolationException("Error");
+        var mockApiRes = new ApiRes<>();
+        mockApiRes.setStatus(false);
+        mockApiRes.setCode(HttpStatus.BAD_REQUEST.value());
+        mockApiRes.setMessage(mockException.getMessage());
+        mockApiRes.setData(null);
+
+        //When
+        when(apiResUtil.returnApiRes(false, HttpStatus.BAD_REQUEST.value(), mockException.getMessage(), null)).thenReturn(mockApiRes);
+        ResponseEntity<?> responseEntity = globaExceptionHandler.handleDataIntegrityViolationException(mockException);
+
+        //Then
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.BAD_REQUEST);
+        assertEquals(responseEntity.getBody(), mockApiRes);
+    }
+
+    @Test
+    void handleValidationTest() {
+
+        //Given
+        FieldError fieldError = mock(FieldError.class);
+        MethodArgumentNotValidException mockException = mock(MethodArgumentNotValidException.class);
+        String enumKey = ErrorCode.UNCATEGORIZED_ERROR.name();
+        ErrorCode errorCode = ErrorCode.valueOf(enumKey);
+        String message = errorCode.getMessage();
+        var mockApiRes = new ApiRes<>();
+        mockApiRes.setStatus(false);
+        mockApiRes.setCode(errorCode.getCode());
+        mockApiRes.setMessage(message);
+        mockApiRes.setData(null);
+
+        //When
+        when(mockException.getFieldError()).thenReturn(fieldError);
+        when(mockException.getFieldError().getDefaultMessage()).thenReturn(enumKey);
+        when(apiResUtil.returnApiRes(false, errorCode.getCode(), message, null)).thenReturn(mockApiRes);
+        ResponseEntity<?> responseEntity = globaExceptionHandler.handleValidation(mockException);
+
+        //Then
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.BAD_REQUEST);
+        assertEquals(responseEntity.getBody(), mockApiRes);
+    }
+
+    @Test
+    void handleFirebaseAuthExceptionTest() {
+
+        //Given
+        FirebaseAuthException mockException = mock(FirebaseAuthException.class);
+        var mockApiRes = new ApiRes<>();
+        mockApiRes.setStatus(false);
+        mockApiRes.setCode(HttpStatus.BAD_REQUEST.value());
+        mockApiRes.setMessage(mockException.getMessage());
+        mockApiRes.setData(null);
+
+        //When
+        when(apiResUtil.returnApiRes(false, HttpStatus.BAD_REQUEST.value(), mockException.getMessage(), null)).thenReturn(mockApiRes);
+        ResponseEntity<?> responseEntity = globaExceptionHandler.handleFirebaseAuthException(mockException);
+
+        //Then
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.BAD_REQUEST);
+        assertEquals(responseEntity.getBody(), mockApiRes);
+        assertEquals(responseEntity.getHeaders().getContentType(), MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    void handleFirebaseAuthExceptionIOTest() {
+
+        //Given
+        IOException mockException = mock(IOException.class);
+        var mockApiRes = new ApiRes<>();
+        mockApiRes.setStatus(false);
+        mockApiRes.setCode(HttpStatus.BAD_REQUEST.value());
+        mockApiRes.setMessage(mockException.getMessage());
+        mockApiRes.setData(null);
+
+        //When
+        when(apiResUtil.returnApiRes(false, HttpStatus.BAD_REQUEST.value(), mockException.getMessage(), null)).thenReturn(mockApiRes);
+        ResponseEntity<?> responseEntity = globaExceptionHandler.handleFirebaseAuthException(mockException);
+
+        //Then
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.BAD_REQUEST);
+        assertEquals(responseEntity.getBody(), mockApiRes);
+    }
+}
--- src/test/java/com/fpt/cursus/util/FileUtilTest.java
+++ src/test/java/com/fpt/cursus/util/FileUtilTest.java
@@ -1,11 +1,98 @@
 package com.fpt.cursus.util;
 
+import org.apache.tika.Tika;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @ContextConfiguration(classes = {FileUtil.class})
 class FileUtilTest {
+
+    @Mock
+    private Tika tika;
+
+    @InjectMocks
+    private FileUtil fileUtil;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getFileFromPathTest() throws IOException {
+
+        //Given
+        File mockFile = File.createTempFile("TestFile", ".txt");
+        FileOutputStream out = new FileOutputStream(mockFile);
+        out.write("Test Content".getBytes());
+
+        //When
+        MultipartFile result = fileUtil.getFileFromPath(mockFile.getPath());
+
+        //Then
+        assertEquals(mockFile.getName(), result.getName());
+        assertEquals("application/octet-stream", result.getContentType());
+        assertEquals("Test Content", new String(result.getBytes()));
+        mockFile.delete();
+    }
+
+    @Test
+    void isImage() throws IOException {
+
+        //Given
+        InputStream inputStream = new ByteArrayInputStream(new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF});
+        MultipartFile mockMultiplepartFile = new MockMultipartFile("TestFile", "TestFile.jpeg", "image/jpeg", inputStream);
+
+        //When
+        boolean result = fileUtil.isImage(mockMultiplepartFile);
+
+        //Then
+        assertTrue(result);
+
+    }
+
+    @Test
+    void isPDF() throws IOException {
+
+        //Given
+        InputStream inputStream = new ByteArrayInputStream("%PDF-1.4\n%".getBytes());
+        MultipartFile mockMultiplepartFile = new MockMultipartFile("TestFile", "TestFile.pdf", "application/pdf", inputStream);
+
+        //When
+        boolean result = fileUtil.isPDF(mockMultiplepartFile);
+
+        //Then
+        assertTrue(result);
+
+    }
+
+    @Test
+    void isVideo() throws IOException {
+
+        //Given
+        InputStream inputStream = new ByteArrayInputStream(new byte[]{0x00, 0x00, 0x00, 0x14, 0x66, 0x74, 0x79, 0x70});
+        MultipartFile mockMultiplepartFile = new MockMultipartFile("TestFile", "TestFile.mp4", "video/mp4", inputStream);
+
+        //When
+        boolean result = fileUtil.isVideo(mockMultiplepartFile);
+
+        //Then
+        assertTrue(result);
+
+    }
+
 
 }
